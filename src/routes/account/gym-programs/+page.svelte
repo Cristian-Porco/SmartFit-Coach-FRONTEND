60edf3ff539e81718aaf6756f6b3827fa993dff9
Cristian Porco <75568774+Cristian-Porco@users.noreply.github.com>
2025-03-06 12:26:56 +0100

    Create gym and food programs page

---
+++ src/routes/account/gym-programs/+page.svelte
@@ -0,0 +1,22 @@
+<head>
+    <style>
+        .icon-sidebar {
+            display: flex;
+        }
+        .sidebar {
+            display: none;
+        }
+        .main-content {
+            min-width: 800px;
+            max-width: 800px;
+        }
+        @media (max-width: 768px) {
+            .main-content {
+                width: 100%;
+                min-width: 100%;
+            }
+        }
+    </style>
+</head>
+
+<h2 class="titlePage">Schede di allenamento</h2>
